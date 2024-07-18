d9da284d6857bc74eec3665d3441a254e17be1b1
Mercymaryantony <mercyantony1112@gmail.com>
2024-07-18 17:27:19 +0530

    signUp completed

--- app.js
+++ app.js
@@ -3,13 +3,39 @@ const Mongoose = require("mongoose")
 const Jwt = require("jsonwebtoken")
 const Bcrypt = require("bcrypt")
 const Cors = require("cors")
+const userModel=require("./models/users")
 
 let app= Express()
 app.use(Express.json())
 app.use(Cors())
 
-app.get("/",(req,res)=>{
-    console.log("hello")
+
+Mongoose.connect("mongodb+srv://mercy1112:mercy1112@cluster0.8x8j3ya.mongodb.net/BloogApp?retryWrites=true&w=majority&appName=Cluster0")
+
+
+app.post("/signUp",(req,res)=>{
+    let input=req.body
+    let hashedPassword=Bcrypt.hashSync(req.body.password,10)
+    console.log(hashedPassword)
+    req.body.password=hashedPassword
+    userModel.find({email:req.body.email}).then(
+        (items)=>{
+            if(items.length>0)
+            {
+                res.json({"status":"Email id already existing"})
+            }
+            else{
+                let result=new userModel(input)
+                result.save()
+                res.json({"status":"Success"})
+            }
+        }
+    ).catch(
+        (error)=>{
+
+        }
+    )
+
 })
 
 app.listen(3030,()=>{
